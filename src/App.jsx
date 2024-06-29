c1911765a850b33c18f690ef2f8c547c7ccd7ebd
Jocelyn Llamas <mjlldlt@hotmail.com>
2024-06-28 19:59:00 -0600

    Footer

--- src/App.jsx
+++ src/App.jsx
@@ -118,9 +118,8 @@ const App = () => {
             {alertMessage}
           </div>
         )}
-
       </div>
-      <footer className="w-full bg-gray-900 text-white py-8">
+      <footer className="w-full bg-gray-900 text-white py-8 mt-28">
         <div className="container mx-auto px-4">
           <div className="flex flex-wrap justify-between">
             <div className="w-full md:w-1/4 mb-6 md:mb-0">
